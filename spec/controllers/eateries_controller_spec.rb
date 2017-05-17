70317dd99a25cabe6341ad8e3728adc4bf9c12a7
Debashish Majumdar <dmajumd2@binghamton.edu>
2017-05-17 07:06:19 +0000

    Rspec done - Test Rspec done - Test Rspec done

--- spec/controllers/eateries_controller_spec.rb
+++ spec/controllers/eateries_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe EateriesController, type: :controller do
+    
+    it 'Restaurant created' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
 
+    it 'Restaurant updated.' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+    
+    it 'Restaurant successfully removed' do
+      get :new
+    expect(response).to have_http_status(200)
+    end
+    
+    it 'Should show categories successfully' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+    
+    it 'this page should show restaurant list ' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+    
+    it 'Restaurant list page should showl search bar ' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
 end
+
+
+
